46cdd4219e97df6e5bbe4622757f69884a9324c1
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2025-02-15 16:02:38 +0100

    updated context file

--- cmd/web/context.go
+++ cmd/web/context.go
@@ -2,4 +2,5 @@ package main
 
 type contextKey string
 
+// isAuthenticatedContextKey is the key used to store the isAuthenticated flag.
 const isAuthenticatedContextKey = contextKey("isAuthenticated")
